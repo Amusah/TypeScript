b870c1654f361b864d258ccbc8f2e0e2470b663e
Amusah <henryamusah@gmail.com>
2025-07-18 21:39:00 +0000

    the Void type

--- functions/functions.ts
+++ functions/functions.ts
@@ -16,33 +16,37 @@ greet("Nerdy");
 
 // Multiple parameters of different types
 const doSomething = (person: string, age: number, isBlessed: boolean) => {};
-doSomething('Henry', 30, true);
-
+doSomething("Henry", 30, true);
 
 //Default parameters
-function salute(person: string = 'Stranger'){
+function salute(person: string = "Stranger") {
   return `Hi there, ${person}!`;
 }
 
-
 // Return Type Annotations
-function calc(num: number = 99):number { // expected to return a number
+function calc(num: number = 99): number {
+  // expected to return a number
   return num * num;
 }
 
-function mystery(num: number){
-  if(Math.random() < 0.5){
+function mystery(num: number) {
+  if (Math.random() < 0.5) {
     return num.toString();
   }
   return num;
 }
 
-const add = (x: number, y: number):number => {
+const add = (x: number, y: number): number => {
   return x + y;
-}
+};
 
-const colors = ['red', 'orange', 'yellow'];
-colors.map(colors => {
+const colors = ["red", "orange", "yellow"];
+colors.map((colors) => {
   return colors.toUpperCase();
   // with this anon func, typscript implicitly infers the parameter type
-})
+});
+
+function printTwice(msg: string): void {
+  console.log(msg);
+  console.log(msg);
+}
