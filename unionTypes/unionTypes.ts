32e278f0304b20d67cd8b62a499980a9f462d995
Amusah <henryamusah@gmail.com>
2025-08-09 20:54:51 +0000

    union Types

---
+++ unionTypes/unionTypes.ts
@@ -0,0 +1,17 @@
+let age: number | string = 21;
+age = 23;
+age = '24'
+
+
+type Point = {
+  x: number;
+  y: number;
+}
+
+type Loc = {
+  lat: number;
+  lon: number;
+}
+
+let coordinates: Point | Loc = {x: 1, y: 34};
+coordinates = {lat: 321.213, lon: 254.3565}
