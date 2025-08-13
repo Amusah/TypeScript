a133a582e1487934b75b5c45b66e235f823d31d8
Amusah <henryamusah@gmail.com>
2025-08-13 20:20:20 +0000

    Literal types

--- unionTypes/unionTypes.ts
+++ unionTypes/unionTypes.ts
@@ -1,17 +1,62 @@
 let age: number | string = 21;
 age = 23;
-age = '24'
-
+age = "24";
 
 type Point = {
   x: number;
   y: number;
-}
+};
 
 type Loc = {
   lat: number;
   lon: number;
+};
+
+let coordinates: Point | Loc = { x: 1, y: 34 };
+coordinates = { lat: 321.213, lon: 254.3565 };
+
+/*************** Type Narrowing union type with functions****************/
+
+function printAge(age: number | string): void {
+  console.log(`You are ${age} years old`);
+}
+printAge(23);
+printAge("77");
+
+function calculateTax(price: number | string, tax: number) {
+  if (typeof price === "string") {
+    price = parseFloat(price.replace("$", ""));
+  } else {
+    price * tax;
+  }
 }
 
-let coordinates: Point | Loc = {x: 1, y: 34};
-coordinates = {lat: 321.213, lon: 254.3565}
+const stuff: (number | string)[] = [1, 2, 3, "henry"];
+
+const coord: (Point | Loc)[] = [];
+
+/********************** Literal Types *******************/
+let zero: 0 = 0;
+// zero = 2;
+
+let hi: "hi" = "hi";
+// hi = 'koo'
+
+let mood: "Happy" | "Sad" | "Mesmerized";
+mood = "Sad";
+// mood = 'angry'
+
+type DayOfWeek =
+  | "Monday"
+  | "Tuesday"
+  | "Wednesday"
+  | "Thusrday"
+  | "Friday"
+  | "Saturday"
+  | "Sunday";
+
+// let today: DayOfWeek = 'weds'
+
+
+
+
