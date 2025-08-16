24ffb40677887d6dfc1b92ea995014ee8e8063e7
Amusah <henryamusah@gmail.com>
2025-08-16 15:48:17 +0000

    Explored enums

--- Tuples_Enums/tuplesEnums.ts
+++ Tuples_Enums/tuplesEnums.ts
@@ -6,7 +6,40 @@
 */
 const color: [number, number, number] = [255, 0, 45];
 
-
 type HTTPResponse = [number, string];
 
-const res: HTTPResponse = [200, 'OK'];
+const res: HTTPResponse = [200, "OK"];
+
+/******************ENUMS***************/
+enum OrderStatus {
+  PENDING,
+  SHIPPED,
+  DELIVERED,
+  RETURNED,
+}
+
+const myStatus = OrderStatus.DELIVERED;
+
+function isDelivered(status: OrderStatus) {
+  return status === OrderStatus.DELIVERED;
+}
+
+isDelivered(OrderStatus.RETURNED);
+
+// Stringed Enums
+enum ArrowKeys {
+  UP = "up",
+  DOWN = "down",
+  LEFT = "left",
+  RIGHT = "right",
+  ERROR = 500
+}
+
+ArrowKeys.RIGHT;
+
+/*
+  Initializing enums with the 'cosnt' keyword
+  strips down codes that floods the namespace
+  at compile time. NB: using 'const' doesn't
+  make it a variable
+*/
