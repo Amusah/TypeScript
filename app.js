88bc40a5a337fe44756747d2dca56252bc67c850
Amusah <henryamusah@gmail.com>
2025-11-09 20:15:58 +0000

    Object types

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
-function add(n1, n2, showResult, phrase) {
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return result;
-    }
-}
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPrase = 'Result is: ';
-add(number1, number2, printResult, resultPrase);
-// console.log(result);
+var person = {
+    name: 'Henry',
+    age: 30
+};
+console.log(person.name);
--- app.ts
+++ app.ts
@@ -1,18 +1,9 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-
-  const result = n1 + n2;
-
-  if(showResult) {
-    console.log(phrase + result);
-  } else {
-    return result;
-  }
+const person: {
+  name: string;
+  age: number
+} = {
+  name: 'Henry',
+  age: 30
 }
 
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPrase = 'Result is: ';
-
-add(number1, number2, printResult, resultPrase);
-// console.log(result);
+console.log(person.name);
