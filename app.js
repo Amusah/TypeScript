0cb18287597641c0664a2a6106199b73ddaad930
Amusah <henryamusah@gmail.com>
2025-11-09 20:34:51 +0000

    Array Types

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[]
+// } = {
 var person = {
     name: 'Henry',
-    age: 30
+    age: 30,
+    hobbies: ['Sports', 'Cooking']
 };
+var favoriteActivities;
+favoriteActivities = ['Sports'];
+favoriteActivities = ['Sports'];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
--- app.ts
+++ app.ts
@@ -1,9 +1,20 @@
-const person: {
-  name: string;
-  age: number
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[]
+// } = {
+
+const person = {
   name: 'Henry',
-  age: 30
-}
+  age: 30,
+  hobbies: ['Sports', 'Cooking']
+};
+
+let favoriteActivities: string[];
+favoriteActivities = ['Sports']
+favoriteActivities = ['Sports'];
 
 console.log(person.name);
+for (const hobby of person.hobbies){
+  console.log(hobby)
+}
