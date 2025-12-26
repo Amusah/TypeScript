849ecaeeb2695942920abe7ee302bd0de8549ab1
Amusah <henryamusah@gmail.com>
2025-12-26 01:14:51 +0000

    funtion return types

--- app.ts
+++ app.ts
@@ -1,32 +1,9 @@
-type Combinable = number | string;
-type ConversionDescriptor = 'as-number' | 'as-text';
+ function add(n1: number, n2: number): number{
+  return n1 + n2;
+ }
 
-function combine(
-  input1: Combinable,
-  input2: Combinable,
-  resultConversion: ConversionDescriptor
-) {
-  let result;
-  if (
-    (typeof input1 === "number" && typeof input2 === "number") ||
-    resultConversion === "as-number"
-  ) {
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
+ function printResult(num: number){
+  console.log('Result: ' + num);
+ }
 
-  if (resultConversion === "as-number") {
-    return +result;
-  } else {
-    return result.toString();
-  }
-}
-
-const combinedAges = combine(30, 26, "as-number");
-console.log(combinedAges);
-
-const combinedStringAges = combine("30", "26", "as-number");
-
-const combinedNames = combine("Henry", "Amusah", "as-text");
-console.log(combinedNames);
+ printResult(add(5, 12));
