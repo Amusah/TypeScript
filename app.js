47da749af0e96b4ad10f347ded6e7f2c70f33c09
Amusah <henryamusah@gmail.com>
2026-01-02 17:04:22 +0000

    Functions as Types

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-function combine(input1, input2) {
-    var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
+function add(n1, n2) {
+    return n1 + n2;
 }
-var combinedAges = combine(30, 26);
-console.log(combinedAges);
-var combinedNames = combine('Henry', 'Amusah');
-console.log(combinedNames);
+function printResult(num) {
+    console.log('Result: ' + num);
+}
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
+printResult(add(5, 12));
+var combineValues;
+combineValues = add;
+console.log(combineValues(10, 10));
--- app.ts
+++ app.ts
@@ -6,4 +6,14 @@
   console.log('Result: ' + num);
  }
 
+ function addAndHandle(n1: number, n2: number, cb: (num: number) => void){
+  const result = n1 + n2;
+  cb(result);
+ }
+
  printResult(add(5, 12));
+
+ let combineValues: (a: number, b: number) => number;
+ combineValues = add;
+
+ console.log(combineValues(10, 10));
