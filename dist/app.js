0fd0dba8411b779ac7147a2e6d0fde43aa894a6f
Amusah <henryamusah@gmail.com>
2026-01-26 00:28:47 +0000

    interfac

--- dist/app.js
+++ dist/app.js
@@ -1,97 +1,11 @@
 "use strict";
-class Department {
-    constructor(id, name) {
-        this.id = id;
-        this.name = name;
-        // public name: string; // default tho
-        // private readonly id: string;
-        // private employees: string[] = [];
-        this.employees = [];
-        // this.name = n;
-    }
-    static createEmployee(name) {
-        return { name: name };
-    }
-    description() {
-        console.log(`Department (${this.id}): ${this.name}`);
-    }
-    addEmployee(employee) {
-        // this.id = 'd2'
-        this.employees.push(employee);
-    }
-    printEmployeeInformation() {
-        console.log(this.employees.length);
-        console.log(this.employees);
-    }
-}
-Department.fiscalYear = 2020;
-class ITDepartment extends Department {
-    constructor(id, admins) {
-        super(id, "IT");
-        this.admins = [];
-        this.admins = admins;
-    }
-}
-const IT = new ITDepartment("d2", ["Henry"]);
-const employee1 = Department.createEmployee("Henry");
-console.log(employee1, Department.fiscalYear);
-console.log(IT);
-IT.description();
-// IT.employees[0] = 'Selassie';
-IT.addEmployee("Henry");
-IT.printEmployeeInformation();
-// const accountingClone = { name: 'Henry', description: accounting.description }
-// accountingClone.description();
-class AccountingDepartment extends Department {
-    constructor(id, reports) {
-        super(id, "Accounting");
-        this.reports = reports;
-        this.lastReport = reports[0];
-    }
-    get mostRecentReport() {
-        if (this.lastReport) {
-            return this.lastReport;
-        }
-        throw new Error("No report found");
-    }
-    static getInstance() {
-        if (this.instance) {
-            return this.instance;
-        }
-        this.instance = new AccountingDepartment("d1", []);
-        return this.instance;
-    }
-    set mostRecentReport(value) {
-        if (!value) {
-            throw new Error("Please pass in a valid value!");
-        }
-        this.addReport(value);
-    }
-    addEmployee(name) {
-        if (name === "Henry") {
-            return;
-        }
-        this.employees.push(name);
-    }
-    addReport(text) {
-        this.reports.push(text);
-        console.log(text);
-    }
-    printReports() {
-        console.log(this.reports);
-    }
-}
-// const accounting = new AccountingDepartment("d1", []); // using private constructor | singleton | static method;
-const accounting = AccountingDepartment.getInstance();
-console.log(accounting);
-accounting.description();
-accounting.mostRecentReport = "end of year report";
-accounting.addReport("we made it");
-console.log(accounting.mostRecentReport);
-accounting.addEmployee("Henry");
-accounting.addEmployee("Barbara");
-// accounting.employees[0] = 'Selassie';
-accounting.addEmployee("Henry");
-accounting.printEmployeeInformation();
-// const accountingClone = { name: 'Henry', description: accounting.description }
-// accountingClone.description();
+let user1;
+user1 = {
+    id: +crypto.randomUUID().split("-")[0],
+    name: "Henry Amusah",
+    age: 30,
+    greet(phrase) {
+        console.log(`${phrase} ${this.name}`);
+    },
+};
+user1.greet('Hey y\'all! this is');
---
+++ src/classes.ts
@@ -0,0 +1,128 @@
+class Department {
+  // public name: string; // default tho
+  // private readonly id: string;
+  // private employees: string[] = [];
+  protected employees: string[] = [];
+
+  constructor(
+    private readonly id: string,
+    public name: string,
+  ) {
+    // this.name = n;
+  }
+
+  static fiscalYear = 2020;
+
+  static createEmployee(name: string) {
+    return { name: name };
+  }
+
+  description(this: Department) {
+    console.log(`Department (${this.id}): ${this.name}`);
+  }
+
+  addEmployee(employee: string) {
+    // this.id = 'd2'
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
+}
+
+class ITDepartment extends Department {
+  public admins: string[] = [];
+  constructor(id: string, admins: string[]) {
+    super(id, "IT");
+    this.admins = admins;
+  }
+}
+const IT = new ITDepartment("d2", ["Henry"]);
+
+const employee1 = Department.createEmployee("Henry");
+console.log(employee1, Department.fiscalYear);
+
+console.log(IT);
+IT.description();
+
+// IT.employees[0] = 'Selassie';
+IT.addEmployee("Henry");
+IT.printEmployeeInformation();
+
+// const accountingClone = { name: 'Henry', description: accounting.description }
+// accountingClone.description();
+
+class AccountingDepartment extends Department {
+  private constructor(
+    id: string,
+    private reports: string[],
+  ) {
+    super(id, "Accounting");
+    this.lastReport = reports[0];
+  }
+
+  private static instance : AccountingDepartment;
+
+  private lastReport: string;
+
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error("No report found");
+  }
+
+  static getInstance(){
+    if(this.instance) {
+      return this.instance;
+    }
+    this.instance = new AccountingDepartment("d1", []);
+    return this.instance;
+  }
+
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error("Please pass in a valid value!");
+    }
+    this.addReport(value);
+  }
+
+  addEmployee(name: string) {
+    if (name === "Henry") {
+      return;
+    }
+    this.employees.push(name);
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+    console.log(text);
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+// const accounting = new AccountingDepartment("d1", []); // using private constructor | singleton | static method;
+const accounting = AccountingDepartment.getInstance();
+
+console.log(accounting);
+accounting.description();
+
+accounting.mostRecentReport = "end of year report";
+
+accounting.addReport("we made it");
+console.log(accounting.mostRecentReport);
+
+accounting.addEmployee("Henry");
+accounting.addEmployee("Barbara");
+
+// accounting.employees[0] = 'Selassie';
+accounting.addEmployee("Henry");
+accounting.printEmployeeInformation();
+
+// const accountingClone = { name: 'Henry', description: accounting.description }
+// accountingClone.description();
